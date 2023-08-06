d30e1f5787d56b14da33e6e6b62c34b1a9d06d7a
Vincent William Rodriguez <33214475+vincentwilliamrodriguez@users.noreply.github.com>
2023-08-06 17:01:30 +0800

    Complete javascript-exercises #4

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removedValues) {
+  for (const val of removedValues) {
+    valIndex = array.indexOf(val);
+    if (valIndex == -1) continue;
+    array.splice(valIndex, 1);
+  }
 
+  return array;
 };
 
 // Do not edit below this line
