d362bbe7bcc39bad5c21ec23f72ec2e8f849441a
Vincent William Rodriguez <33214475+vincentwilliamrodriguez@users.noreply.github.com>
2023-08-09 16:23:35 +0800

    Complete JavaScript Exercise #12

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const getAge = (person) => {
+    let end = (person.yearOfDeath !== undefined) ? person.yearOfDeath
+                                                 : (new Date()).getFullYear();
+    return (end - person.yearOfBirth);
+  };
 
+  return people.reduce((oldest, person) => {
+    return (getAge(person) > getAge(oldest)) ? person : oldest;
+  });
 };
+
+findTheOldest([
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]);
 
 // Do not edit below this line
 module.exports = findTheOldest;
