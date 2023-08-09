352e13ce421cef9a26ccd7965699937482f15a08
Vincent William Rodriguez <33214475+vincentwilliamrodriguez@users.noreply.github.com>
2023-08-09 15:53:42 +0800

    Complete JavaScript Exercise #9

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  const filtered = text.toLowerCase()
+                       .split('')
+                       .filter(char => /^[a-z0-9]+$/.test(char));
+  
+  return filtered.join('') === filtered.reverse().join('');
 };
 
 // Do not edit below this line
