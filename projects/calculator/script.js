8e839d727d13082f261e0636577d82c3499b4abf
Vincent William Rodriguez <33214475+vincentwilliamrodriguez@users.noreply.github.com>
2023-08-10 15:48:51 +0800

    Add display functionality to numbers and AC/DEL buttons (calculator)

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -1,8 +1,40 @@
+const numberBtns = document.querySelectorAll(".number");
+const operationBtns = document.querySelectorAll(".operation");
+const clearBtn = document.querySelector(".clear");
+const deleteBtn = document.querySelector(".delete");
+const pointBtn = document.querySelector(".point");
+const display = document.querySelector(".display");
 
 const add =       (a, b) => (a + b);
 const subtract =  (a, b) => (a - b);
 const multiply =  (a, b) => (a * b);
 const divide =    (a, b) => (a / b);
+
+initialize();
+
+function initialize() {
+  // Number Buttons
+  for (const numberBtn of numberBtns) {
+    numberBtn.addEventListener("click", (e) => {
+      display.textContent += e.target.dataset.num;
+    });
+  }
+
+  // Clear and Delete Buttons
+  clearBtn.addEventListener("click", (e) => {
+    display.textContent = "";
+  });
+
+  deleteBtn.addEventListener("click", (e) => {
+    display.textContent = display.textContent.slice(0, -1);
+  });
+
+  // Point Button
+
+  // Operation Buttons
+}
+
+
 
 function operate(first, operator, second) {
   first = +first;
