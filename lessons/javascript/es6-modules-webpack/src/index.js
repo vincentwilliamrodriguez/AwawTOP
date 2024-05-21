dc1aa8edacf8650cbf03a459e32dee4b1523d7e4
Vincent William Rodriguez <33214475+vincentwilliamrodriguez@users.noreply.github.com>
2024-05-21 14:39:57 +0800

    Code along until Webpack Development part

--- lessons/javascript/es6-modules-webpack/src/index.js
+++ lessons/javascript/es6-modules-webpack/src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
+import "./style.scss";
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+
   return element;
 }
 
  document.body.appendChild(component());
+ console.log('awawawaw!');
--- lessons/javascript/es6-modules-webpack/webpack.config.js
+++ lessons/javascript/es6-modules-webpack/webpack.config.js
@@ -1,9 +1,42 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  watch: true,
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    watchFiles: ['src/**/index.html'],
+    hot: true,
+  },
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "head",
+      scriptLoading: "defer",
+    })
+  ]
 };
