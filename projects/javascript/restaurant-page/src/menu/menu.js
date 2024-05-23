13e69d729438db4fcbc911d7062e17aae9fc8522
Vincent William Rodriguez <33214475+vincentwilliamrodriguez@users.noreply.github.com>
2024-05-23 15:36:15 +0800

    (Restaurant Page) Fixed menu typo

--- projects/javascript/restaurant-page/src/menu/menu.js
+++ projects/javascript/restaurant-page/src/menu/menu.js
@@ -21,7 +21,7 @@ export default function MenuElement () {
 
   const options = [
     new Option("Pollos-style Fried Chicken", "Enjoy our original chicken cooked to perfection for your needs.", "pollos-style-chicken.jpg"),
-    new Option("Curly Fries", "t may not be truly curly, but our fries contains a secret ingredient spices things up!", "curly-fries.jpg"),
+    new Option("Curly Fries", "It may not be truly curly, but our fries contains a secret ingredient spices things up!", "curly-fries.jpg"),
     new Option("Egg Sandwich", "This sandwich made from fresh Pollos eggs is sure to tickle your taste buds.", "egg-sandwich.jpg"),
     new Option("Veggie Bacon Burger", "Have a son that loves veggies for breakfast? Our Veggie Bacon Burger has you covered!", "veggie-bacon-burger.jpg"),
     new Option("Homemade Iced Tea", "Sourced from the natural leaves of Mexico, this iced tea is deemed to be 99.1% pure!", "homemade-iced-tea.jpg"),
