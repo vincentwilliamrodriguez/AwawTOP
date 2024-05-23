a902d71527c15ca7a18e6405dce914c60ec50371
Vincent William Rodriguez <33214475+vincentwilliamrodriguez@users.noreply.github.com>
2024-05-23 15:05:26 +0800

    (Restaurant Page) Finalize project

--- projects/javascript/restaurant-page/src/index.js
+++ projects/javascript/restaurant-page/src/index.js
@@ -7,9 +7,37 @@ import ContactElement from "./contact/contact.js";
 
 const DisplayManager = (function () {
   const contentElement = document.querySelector("#content");
+  const tabs = {
+    home: {
+      button: document.querySelector("#home-btn"),
+      tabElement: HomeElement
+    },
+    menu: {
+      button: document.querySelector("#menu-btn"),
+      tabElement: MenuElement
+    },
+    contact: {
+      button: document.querySelector("#contact-btn"),
+      tabElement: ContactElement
+    },
+  }
 
   function init() {
-    contentElement.appendChild(ContactElement());
+    for (const [tabName, tabData] of Object.entries(tabs)) {
+      tabData.button.addEventListener("click", (e) => {
+        updateTab(tabName);
+      });
+    }
+
+    updateTab("home");
+  }
+
+  function updateTab(tabName) {
+    document.querySelector(".navbar__btn--active").classList.remove("navbar__btn--active");
+    tabs[tabName].button.classList.add("navbar__btn--active");
+
+    contentElement.innerHTML = "";
+    contentElement.appendChild(tabs[tabName].tabElement());
   }
 
   return {
