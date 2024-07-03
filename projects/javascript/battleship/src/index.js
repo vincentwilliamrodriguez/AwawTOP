60b6047b01a17174f694a6cb23d98439239042b1
Vincent William Rodriguez <vincentwilliamr@gmail.com>
2024-07-03 14:33:29 +0800

    feat(battleship): accomplish CSS for fog and small fog

--- projects/javascript/battleship/src/index.js
+++ projects/javascript/battleship/src/index.js
@@ -4,6 +4,11 @@ import DisplayManager from './display.js'
 import * as Helper from './helper.mjs';
 
 window.globals = {};
+
+const $ = document.querySelector.bind(document);
+
+$('.modal').showModal();
+
 window.globals.Game = DisplayManager.Game
 window.globals.DisplayManager = DisplayManager
 
