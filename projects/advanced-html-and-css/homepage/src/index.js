b335b6a5cbce6e02c045dcab7c45280d0b50d60e
Vincent William Rodriguez <vincentwilliamr@gmail.com>
2024-07-31 13:59:00 +0800

    feat(homepage): create CSS for tablet sizes

--- projects/advanced-html-and-css/homepage/src/index.js
+++ projects/advanced-html-and-css/homepage/src/index.js
@@ -1,3 +1,23 @@
 // window.globals = {};
 
 import './style.scss';
+
+// const $ = document.querySelector.bind(document);
+// const $$ = document.querySelectorAll.bind(document);
+
+// window.addEventListener('load', updateScaleSize);
+// window.addEventListener('resize', updateScaleSize);
+
+// function updateScaleSize() {
+//   if (window.innerWidth >= 600) {
+//     for (const participantElem of $$('.participant')) {
+//       const newWidth = 0.7 * getComputedStyle(participantElem).getPropertyValue('--width');
+//       const newHeight = 0.7 * getComputedStyle(participantElem).getPropertyValue('--height');
+
+//       console.log('awaw', getComputedStyle(participantElem).getPropertyValue('height'))
+
+//       participantElem.style.width = `${newWidth}px`;
+//       participantElem.style.height = `${newHeight}px`;
+//     }
+//   }
+// }
